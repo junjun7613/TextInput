241e1f28534a2009d30640928c47290ad43cc73e
nakamura196 <na.kamura.1263@gmail.com>
2022-05-23 18:08:44 +0900

    feat: add function to load and update firebase entry

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <p>{{ex_text}}</p>
-    <p>selected words: {{selected_word_start_id}} - {{selected_word_end_id}}</p>
+    <p>{{ ex_text }}</p>
+    <p>
+      selected words: {{ selected_word_start_id }} - {{ selected_word_end_id }}
+    </p>
     <div fluid>
       <div>
         <div id="textDiv" :style="`height: 600px; overflow-y: auto`">
-          <TEI v-if="element" :element="element" @parent-func="getWId"/>
+          <TEI v-if="element" :element="element" @parent-func="getWId" />
         </div>
       </div>
-    </div> 
+    </div>
     <!--<v-container fluid>
       <v-row>
         <v-col id="textDiv" :style="`height: ${height}px; overflow-y: auto`">
@@ -21,12 +23,16 @@
     </template> -->
     <v-container fluid v-if="false">
       <v-row>
-        <v-col v-for="item in w_id_list" :key="item.id" @click="clickText(item.id)">
-          {{item.text}}
+        <v-col
+          v-for="item in w_id_list"
+          :key="item.id"
+          @click="clickText(item.id)"
+        >
+          {{ item.text }}
         </v-col>
       </v-row>
     </v-container>
-    {{WId}}
+    {{ WId }}
     <select id="listbox" v-model="selected">
       <option value="pers">Person</option>
       <option value="place">Place</option>
@@ -35,21 +41,29 @@
     </select>
     <!--<div>selected: {{selected}}</div>-->
     <button @click="selectEntity">select</button>
-    {{ids}}
+    {{ ids }}
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import { getFirestore, collection, addDoc } from 'firebase/firestore'
+import axios from "axios";
+import {
+  doc,
+  getDoc,
+  getFirestore,
+  collection,
+  addDoc,
+  updateDoc,
+} from "firebase/firestore";
 
-const convert = require('xml-js')
+const convert = require("xml-js");
 //const { scroller } = require('vue-scrollto/src/scrollTo')
 
 export default {
-  layout: 'noFooter',
+  layout: "noFooter",
   data() {
     return {
+      documentId: "",
       element: null,
       text: "",
       w_id_list: null,
@@ -57,85 +71,65 @@ export default {
       //df: null,
       WId: "",
       selected: "",
-      selectedEntity: "",
+      //selectedEntity: "",
       wids: [],
       ids: null,
-      firestoreId: '001',
-      firestoreName: 'test'
+      firestoreId: "001",
+      firestoreName: "test",
       //height: window.innerHeight - 64,
-    }
+    };
   },
   computed: {
     ex_text: {
       get() {
-        return this.$store.getters.getExText
+        return this.$store.getters.getExText;
       },
       set(value) {
-        this.$store.commit('setExText', value)
-      }
+        this.$store.commit("setExText", value);
+      },
     },
     selected_word_start_id: {
       get() {
-        return this.$store.getters.getSelectedWordStartId
+        return this.$store.getters.getSelectedWordStartId;
       },
       set(value) {
-        this.$store.commit('setSelectedWordStartId', value)
-      }
+        this.$store.commit("setSelectedWordStartId", value);
+      },
     },
     selected_word_end_id: {
       get() {
-        return this.$store.getters.getSelectedWordEndId
+        return this.$store.getters.getSelectedWordEndId;
       },
       set(value) {
-        this.$store.commit('setSelectedWordEndId', value)
-      }
-    }
+        this.$store.commit("setSelectedWordEndId", value);
+      },
+    },
   },
   watch: {
-    data: function(){
-      console.log(this.WId)
-    } 
+    data: function () {
+      console.log(this.WId);
+    },
   },
   async mounted() {
-    const res = await axios.get("/TEI/BG_1_TEI_final.xml")
-    console.log(typeof res)
-    this.xmlData = res
+    const db = getFirestore();
 
-    const parser = new window.DOMParser()
-    const xmlData = parser.parseFromString(res.data, 'text/xml')
-    //this.xmlData = xmlData
+    const documentId = "VG34DNwDkzFVlk47u7jS";
+    this.documentId = documentId;
 
-    // テスト
-    const ids = ["w_1_1_2_12", "w_1_1_2_13"]
-    //const ids = this.ids
-    console.log(ids)
+    const docRef = doc(db, "tasks", documentId);
+    const docSnap = await getDoc(docRef);
 
-
-    let element_name = "persName"
-    let element_prefix = "pers"
-    let element_id = ids[0].replace("w_", `${element_prefix}_`)
-    if(ids.length > 1){
-      const last_id = ids[ids.length - 1]
-      const last_id_spl = last_id.split("_")
-      element_id += "-" + last_id_spl[last_id_spl.length - 1]
+    let xmlStr = "";
+    if (docSnap.exists()) {
+      xmlStr = docSnap.data().xml;
+    } else {
+      const res = await axios.get("/TEI/BG_1_TEI_final.xml");
+      xmlStr = res.data;
     }
 
-    let elementAdded = null
-    for(let i = 0; i < ids.length; i++){
-      const id = ids[i]
-      const wordElement = xmlData.querySelector(`[*|id="${id}"]`)
-
-      if(i == 0){
-        elementAdded = document.createElement(element_name)
-        elementAdded.setAttribute("xml:id", element_id)
-        //対象のw要素の前に挿入
-        wordElement.parentNode.insertBefore(elementAdded, wordElement)
-      }
-
-      //対象のw要素をpersNameの子要素として挿入
-      elementAdded.appendChild(wordElement)
-    }
-
+    const parser = new window.DOMParser();
+    const xmlData = parser.parseFromString(xmlStr, "text/xml");
+    this.xmlData = xmlData;
 
     /*
     const wordElement = xmlData.querySelector(`[*|id="w_1_1_2_12"]`)
@@ -147,56 +141,56 @@ export default {
     //対象のw要素をpersNameの子要素として挿入
     persName.appendChild(wordElement)
     */
-    this.xmlData = xmlData
+  },
+  watch: {
+    xmlData: function () {
+      const xmlData = this.xmlData;
+      const df = JSON.parse(
+        convert.xml2json(xmlData.querySelector("text").outerHTML, {
+          compact: false,
+          spaces: 4,
+        })
+      );
 
-    const df = JSON.parse(
-      convert.xml2json(this.xmlData.querySelector('text').outerHTML, {
-        compact: false,
-        spaces: 4,
-      })
-    )
+      // idの一覧を取得する
+      const ws = xmlData.querySelectorAll("w");
 
-    //this.df = df
+      const wids = [];
+      for (const w of ws) {
+        const id = w.getAttribute("xml:id");
+        wids.push(id);
+      }
 
-    // idの一覧を取得する
-    const ws = xmlData.querySelectorAll('w')
+      console.log(wids);
+      this.wids = wids;
 
-    const wids = []
-    for (const w of ws) {
-      const id = w.getAttribute('xml:id')
-      wids.push(id)
-    }
+      const w_id_list = [];
+      //const words = []
 
-    console.log(wids)
-    this.wids = wids
+      for (const wid of wids) {
+        //console.log(wid)
+        const w_id = {};
+        w_id["id"] = wid;
+        const wordText = xmlData.querySelector(`w[*|id='${wid}']`);
+        //console.log(wordText.getAttribute("lemma"))
+        const word = wordText.textContent.trim();
+        //words.push(wordText)
+        w_id["text"] = word;
+        w_id_list.push(w_id);
+      }
 
-    const w_id_list = []
-    //const words = []
+      console.log(w_id_list);
 
-    for (const wid of wids) {
-      //console.log(wid)
-      const w_id = {}
-      w_id["id"] = wid
-      const wordText = xmlData.querySelector(`w[*|id='${wid}']`)
-      //console.log(wordText.getAttribute("lemma"))
-      const word = wordText.textContent.trim()
-      //words.push(wordText)
-      w_id["text"] = word
-      w_id_list.push(w_id)
-    }
+      //this.text = words.join(' ')
+      this.w_id_list = w_id_list;
 
-    console.log(w_id_list)
-
-    //this.text = words.join(' ')
-    this.w_id_list = w_id_list
-
-    this.element = df
-    //console.log(typeof df)
-
+      this.element = df;
+      //console.log(typeof df)
+    },
   },
   methods: {
-    clickText(value){
-      console.log(value)
+    clickText(value) {
+      console.log(value);
     },
     /*scroll(id) {
       if (!id) {
@@ -213,50 +207,94 @@ export default {
         console.log({ e })
       }
     }, */
-    getWId(value){
-      console.log(value)
+    getWId(value) {
+      console.log(value);
       this.WId = value;
     },
-    selectEntity(){
+    async selectEntity() {
+      const selected = this.selected;
+      //this.selectedEntity = selected;
 
-      const selected = this.selected
-      this.selectedEntity = selected
+      let xmlData = this.xmlData;
+      const wids = this.wids;
+      console.log(wids);
 
-      const xmlData = this.xmlData
-      const wids = this.wids
-      console.log(wids)
-
-      const idStart = this.$store.getters.getSelectedWordStartId
+      const idStart = this.$store.getters.getSelectedWordStartId;
       //console.log(typeof idStart)
-      const indexStart = wids.indexOf(idStart)
-      const idEnd = this.$store.getters.getSelectedWordEndId
-      const indexEnd = wids.indexOf(idEnd)
+      const indexStart = wids.indexOf(idStart);
+      const idEnd = this.$store.getters.getSelectedWordEndId;
+      const indexEnd = wids.indexOf(idEnd);
 
-      console.log(this.selectedEntity)
-      console.log(indexStart)
-      console.log(indexEnd)
-      console.log(xmlData)
+      console.log(/*this.selectedEntity*/ selected);
+      console.log(indexStart);
+      console.log(indexEnd);
+      console.log(xmlData);
 
-      const ids = []
-      const start = indexStart
-      const end = indexEnd
-      const indexList = new Array(end - start + 1).fill(null).map((_, i) => i + start)
+      const ids = [];
+      const start = indexStart;
+      const end = indexEnd;
+      const indexList = new Array(Math.abs(end - start) + 1)
+        .fill(null)
+        .map((_, i) => i + start);
       //console.log(indexList)
-      for (const index of indexList){
+      for (const index of indexList) {
         //console.log(wids[index])
-        ids.push(wids[index])
+        ids.push(wids[index]);
       }
-      this.ids = ids
+      //this.ids = ids;
 
-      const db = getFirestore()
-      const docRef = addDoc(collection(db, 'tasks'), {
-        //id: this.firestoreId,
-        //name: this.firestoreName,
-        words: this.w_id_list
-      })
-      console.log('Document written with ID: ', docRef.id)
+      const db = getFirestore();
 
-    }
-  }
-}
+      //更新
+      xmlData = this.updateTest(xmlData, ids, selected);
+      this.xmlData = xmlData;
+
+      // 文字列に変換して、firestoreに保存
+      var xmlSerializer = new XMLSerializer();
+      var xmlString = xmlSerializer.serializeToString(xmlData);
+
+      //console.log(xmlString)
+
+      const docRef = doc(db, "tasks", this.documentId);
+      await updateDoc(docRef, {
+        xml: xmlString,
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+    },
+    //テストの更新用関数
+    updateTest(xmlData, ids, selected) {
+      // テスト
+      //const ids = ["w_1_1_2_12", "w_1_1_2_13"];
+      //const ids = this.ids
+
+      //let element_name = "persName";
+      let element_prefix = selected; //"pers";
+      let element_name = `${element_prefix}Name`;
+      let element_id = ids[0].replace("w_", `${element_prefix}_`);
+      if (ids.length > 1) {
+        const last_id = ids[ids.length - 1];
+        const last_id_spl = last_id.split("_");
+        element_id += "-" + last_id_spl[last_id_spl.length - 1];
+      }
+
+      let elementAdded = null;
+      for (let i = 0; i < ids.length; i++) {
+        const id = ids[i];
+        const wordElement = xmlData.querySelector(`[*|id="${id}"]`);
+
+        if (i == 0) {
+          elementAdded = xmlData.createElement(element_name);
+          elementAdded.setAttribute("xml:id", element_id);
+          //対象のw要素の前に挿入
+          wordElement.parentNode.insertBefore(elementAdded, wordElement);
+        }
+
+        //対象のw要素をpersNameの子要素として挿入
+        elementAdded.appendChild(wordElement);
+      }
+      return xmlData;
+    },
+  },
+};
 </script>

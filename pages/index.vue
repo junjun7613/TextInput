1dd6159df26d41d78911fe76898afdab53e03fb4
Jun Ogawa <junogawa@junnoMacBook-Pro.local>
2022-06-22 18:56:43 +0900

    feat: modified index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,40 @@
     <nuxt-link to="/markup" style="font-size: 20pt;">Do markup</nuxt-link>
     <br/>
     <nuxt-link to="/linkedData" style="font-size: 20pt;">Create linked data</nuxt-link>
+    <br/>
+    <br/>
+    <button @click="getTTL">download TTL file</button>
   </div>
 </template>
+<script>
+import axios from "axios";
+import {
+  doc,
+  getDoc,
+  setDoc,
+  getFirestore,
+  collection,
+  addDoc,
+  updateDoc,
+} from "firebase/firestore";
+
+export default {
+  layout: "noFooter",
+  data() {},
+  computed: {},
+  async mounted() {},
+  watch: {},
+  methods: {
+    async getTTL(){
+      const db = getFirestore();
+      const docRef = doc(db, "lod", "0c31f3c8-8b4c-415b-9bc4-bc8294f1f8e3");
+      //const docRef = doc(db, "lod");
+      const docSnap = await getDoc(docRef);
+      //const docSnap = db.collection("lod").getDoc()
+      console.log(docSnap.data())
+    }
+  },
+};
+</script>
+
 

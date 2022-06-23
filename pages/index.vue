d47ae8b9b98a5e63119ebaac53428547c54c1e29
Satoru <you@example.com>
2022-06-23 14:56:56 +0900

    feat: add function to download ttl

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,78 @@
 <template>
   <div>
-    <nuxt-link to="/markup" style="font-size: 20pt;">Do markup</nuxt-link>
-    <br/>
-    <nuxt-link to="/linkedData" style="font-size: 20pt;">Create linked data</nuxt-link>
-    <br/>
-    <br/>
+    <nuxt-link to="/markup" style="font-size: 20pt">Do markup</nuxt-link>
+    <br />
+    <nuxt-link to="/linkedData" style="font-size: 20pt"
+      >Create linked data</nuxt-link
+    >
+    <br />
+    <br />
     <button @click="getTTL">download TTL file</button>
   </div>
 </template>
 <script>
-import axios from "axios";
+
+const N3 = require("n3");
+
+const { DataFactory } = N3;
+const { namedNode } = DataFactory;
+
 import {
-  doc,
-  getDoc,
-  setDoc,
   getFirestore,
   collection,
-  addDoc,
-  updateDoc,
+  getDocs,
 } from "firebase/firestore";
 
 export default {
   layout: "noFooter",
-  data() {},
+  data() {
+    return {}
+  },
   computed: {},
-  async mounted() {},
+  async mounted() {
+  },
   watch: {},
   methods: {
-    async getTTL(){
+    async getTTL() {
+
+      const c = "http://example.org/cartoons#"
+
+      const writer = new N3.Writer({
+        prefixes: { c },
+      });
+
       const db = getFirestore();
+      /*
       const docRef = doc(db, "lod", "0c31f3c8-8b4c-415b-9bc4-bc8294f1f8e3");
       //const docRef = doc(db, "lod");
       const docSnap = await getDoc(docRef);
       //const docSnap = db.collection("lod").getDoc()
       console.log(docSnap.data())
-    }
+      */
+      const lodRef = collection(db, "lod");
+      const querySnapshot = await getDocs(lodRef);
+      querySnapshot.forEach((doc) => {
+        const jsonTriples = doc.data().jsonTriples
+
+        writer.addQuad(
+          namedNode(`${c}${jsonTriples.id}`),
+          namedNode("http://www.w3.org/1999/02/22-rdf-syntax-ns#type"),
+          namedNode(`${c}${jsonTriples.type}`)
+        );
+      });
+
+      download(writer, "test.ttl")
+    },
   },
 };
+
+function download(writer, filename){
+  writer.end((error, result) => {
+    var element = document.createElement('a');
+    element.setAttribute('href','data:text/plain;charset=utf-8, ' + encodeURIComponent(result));
+    element.setAttribute('download', filename);
+    document.body.appendChild(element);
+    element.click();
+  });
+}
 </script>
-
-
